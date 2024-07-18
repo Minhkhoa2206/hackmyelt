db15edc6afd7011af9d459e2a50250c33142dda1
Minh Khoa <97280663+Minhkhoa2206@users.noreply.github.com>
2024-07-18 15:43:32 +0700

    Create code.js

---
+++ code.js
@@ -0,0 +1,10 @@
+// Tìm tất cả các nút có lớp 'show-answer-button'
+const buttons = document.querySelectorAll('.show-answer-button');
+// Duyệt qua tất cả các nút và hiển thị chúng
+buttons.forEach(button => {
+  // Bỏ thuộc tính 'ng-hide' 
+  button.classList.remove('ng-hide');
+  
+  // Bỏ 'disabled' để kích hoạt nút
+  button.removeAttribute('disabled');
+});
